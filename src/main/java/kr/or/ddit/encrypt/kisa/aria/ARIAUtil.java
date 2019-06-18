744cc8dfce724a7848d42c909b8f6d3599c68f62
Deviyu <deviyu091@gmail.com>
2019-06-18 11:19:16 +0900

    인코딩 문제 수정

--- src/main/java/kr/or/ddit/encrypt/kisa/aria/ARIAUtil.java
+++ src/main/java/kr/or/ddit/encrypt/kisa/aria/ARIAUtil.java
@@ -1,4 +1,4 @@
-﻿package kr.or.ddit.encrypt.kisa.aria;
+package kr.or.ddit.encrypt.kisa.aria;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -80,7 +80,6 @@ public class ARIAUtil {
 	}
 
 	/*
-	 *Aria 복호화
 	 */
 	public static String ariaDecrypt(String strHex, String privateKey) 
 	throws InvalidKeyException, UnsupportedEncodingException  {
@@ -153,7 +152,6 @@ public class ARIAUtil {
 
 
 	/*
-	 *Aria 기본 복호화
 	 */
 	public static String ariaDecrypt(String strHex) 
 	throws InvalidKeyException, UnsupportedEncodingException  {
@@ -180,7 +178,6 @@ public class ARIAUtil {
 		}
 	}
 	/*
-	*aria 기본 암호화
 	*/
 	public static String ariaEncrypt(String str) 
 	throws InvalidKeyException, UnsupportedEncodingException {
@@ -205,7 +202,6 @@ public class ARIAUtil {
 	}
 	
 	/*
-	*캐릭터셋 변경 암호화
 	*/
 	public static String ariaCharEncrypt(String str, String charset) 
 	throws InvalidKeyException, UnsupportedEncodingException {
@@ -228,8 +224,6 @@ public class ARIAUtil {
 	}
 	
 	/*
-	*캐릭터셋 변경 암호화
-	*(서버타입설정 열람서버 : read, 등록관리서버 : regi)
 	*/
 	public static String ariaCharEncrypt(String str, String charset, String server) 
 	throws InvalidKeyException, UnsupportedEncodingException {
@@ -257,7 +251,6 @@ public class ARIAUtil {
 
 
 	/*
-	 *Aria 캐릭터셋 변경 복호화
 	 */
 	public static String ariaCharDecrypt(String strHex,  String charset) 
 	throws InvalidKeyException, UnsupportedEncodingException  {
@@ -279,8 +272,6 @@ public class ARIAUtil {
 	}
 	
 	/*
-	 *Aria 캐릭터셋 변경 복호화
-	 *(서버타입설정 열람서버 : read, 등록관리서버 : regi)
 	 */
 	public static String ariaCharDecrypt(String strHex,  String charset, String server) 
 	throws InvalidKeyException, UnsupportedEncodingException  {
